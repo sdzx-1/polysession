138bc3ee49485165a09888a7eb3bdf5c917d89e2
sdzx-1 <shangdizhixia1993@163.com>
2025-08-30 15:32:08 +0800

    Add Other

--- src/root.zig
+++ src/root.zig
@@ -12,13 +12,22 @@ pub fn Session(
         data: Data_,
 
         pub const name = name_;
+
         pub const State = State_;
         pub const Data = Data_;
     };
 }
 
-pub fn PingPong(State_: type, Data_: type) type {
-    return Session("PingPong", State_, Data_);
+pub fn Other(
+    comptime OtherState_: type,
+    comptime Data_: type,
+) type {
+    return struct {
+        data: Data_,
+
+        pub const OtherState = OtherState_;
+        pub const Data = Data_;
+    };
 }
 
 // pub const RC = union(enum) {
@@ -27,11 +36,15 @@ pub fn PingPong(State_: type, Data_: type) type {
 //     c: i32,
 // };
 
+pub fn PingPong(State_: type, Data_: type) type {
+    return Session("PingPong", State_, Data_);
+}
+
 //RC: RoleAndContext
 pub fn Idle(RC: type, From_: meta.Tag(RC), To_: meta.Tag(RC), Next: type) type {
     return union(enum) {
         ping: PingPong(Busy(RC, To_, From_, Next), i32),
-        next: PingPong(Next, void),
+        next: Other(Next, void),
 
         pub const From = From_;
         pub const To = To_;
