9bebb28019d1cb9a332507ee97c0c4fb5dde6f1f
sdzx-1 <shangdizhixia1993@163.com>
2025-09-02 11:56:01 +0800

    Impl example

--- src/root.zig
+++ src/root.zig
@@ -18,64 +18,58 @@ pub fn Session(
     };
 }
 
-pub fn Other(
-    comptime OtherState_: type,
-    comptime Data_: type,
-) type {
-    return struct {
-        data: Data_,
+pub const Role = enum { client, server };
 
-        pub const OtherState = OtherState_;
-        pub const Data = Data_;
-    };
-}
-
-// pub const RC = union(enum) {
-//     a: i32,
-//     b: i32,
-//     c: i32,
-// };
+//example
 
 pub fn PingPong(State_: type, Data_: type) type {
     return Session("PingPong", State_, Data_);
 }
 
-//RC: RoleAndContext
-pub fn Idle(RC: type, From_: meta.Tag(RC), To_: meta.Tag(RC), Next: type) type {
-    return union(enum) {
-        ping: PingPong(Busy(RC, To_, From_, Next), i32),
-        next: Other(Next, void),
+pub const ServerContext = struct {
+    server_counter: i32,
+};
 
-        pub const From = From_;
-        pub const To = To_;
+pub const ClientContext = struct {
+    client_counter: i32,
+};
 
-        pub fn send(ctx: *meta.TagPayload(RC, From)) @This() {
-            ctx.* += 1;
-            return .{ .ping = .{ .data = 10 } };
+pub const Idle = union(enum) {
+    ping: PingPong(Busy, i32),
+    exit: PingPong(Exit, void),
+
+    pub const Agency: Role = .client;
+
+    pub fn process(ctx: *ClientContext) @This() {
+        if (ctx.client_counter > 100) return .{ .exit = .{ .data = {} } };
+        return .{ .ping = .{ .data = ctx.client_counter } };
+    }
+
+    pub fn preprocess(ctx: *ServerContext, msg: @This()) void {
+        switch (msg) {
+            .exit => {},
+            .ping => |val| {
+                ctx.server_counter = val.data;
+            },
         }
+    }
+};
 
-        pub fn recv(ctx: *meta.TagPayload(RC, To), msg: @This()) void {
-            _ = ctx;
-            _ = msg;
+pub const Busy = union(enum) {
+    pong: PingPong(Idle, i32),
+
+    pub const Agency: Role = .server;
+
+    pub fn process(ctx: *ServerContext) @This() {
+        ctx.server_counter += 1;
+        return .{ .pong = .{ .data = ctx.server_counter } };
+    }
+
+    pub fn preprocess(ctx: *ClientContext, msg: @This()) void {
+        switch (msg) {
+            .pong => |val| {
+                ctx.client_counter = val.data;
+            },
         }
-    };
-}
-
-pub fn Busy(RC: type, From_: meta.Tag(RC), To_: meta.Tag(RC), Next: type) type {
-    return union(enum) {
-        pong: PingPong(Idle(RC, To_, From_, Next), i32),
-
-        pub const From = From_;
-        pub const To = To_;
-
-        pub fn send(ctx: *meta.TagPayload(RC, From)) @This() {
-            _ = ctx;
-            return .{ .pong = .{ .data = 10 } };
-        }
-
-        pub fn recv(ctx: *meta.TagPayload(RC, To), msg: @This()) void {
-            _ = ctx;
-            _ = msg;
-        }
-    };
-}
+    }
+};
