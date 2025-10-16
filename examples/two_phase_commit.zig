8999fa5f59941c2b53802ccbbd95ec5ddab73ba2
sdzx-1 <shangdizhixia1993@163.com>
2025-10-16 10:13:19 +0800

    Impl two_phase_commit.zig

--- examples/two_phase_commit.zig
+++ examples/two_phase_commit.zig
@@ -3,8 +3,108 @@ const ps = @import("polysession");
 const Data = ps.Data;
 const StreamChannel = @import("channel.zig").StreamChannel;
 
-pub const Role = enum { alice, bob, charlie };
-
 pub fn main() !void {
-    std.debug.print("nice\n", .{});
+    std.debug.print("curr_id {d}\n", .{curr_id});
 }
+
+pub const Runner = ps.Runner(Start);
+pub const curr_id = Runner.idFromState(Start);
+
+//
+const Role = enum { coordinator, alice, bob };
+
+const Context = struct {
+    coordinator: type = u32,
+    alice: type = struct { xoshiro256: std.Random.Xoshiro256 },
+    bob: type = struct { xoshiro256: std.Random.Xoshiro256 },
+};
+
+fn two_pc(sender: Role, receiver: []const Role) ps.ProtocolInfo("2pc", Role, Context{}) {
+    return .{ .sender = sender, .receiver = receiver };
+}
+
+const Start = union(enum) {
+    begin: Data(void, AliceResp),
+
+    pub const info = two_pc(.coordinator, &.{ .alice, .bob });
+
+    pub fn process(ctx: *Context.coordinator) !@This() {
+        _ = ctx;
+        return .{ .begin = .{ .data = {} } };
+    }
+
+    pub fn preprocess_0(ctx: *Context.alice, msg: @This()) !void {
+        _ = ctx;
+        _ = msg;
+    }
+
+    pub fn preprocess_1(ctx: *Context.bob, msg: @This()) !void {
+        _ = ctx;
+        _ = msg;
+    }
+};
+
+const AliceResp = union(enum) {
+    resp: Data(bool, BobResp),
+
+    pub const info = two_pc(.alice, &.{.coordinator});
+
+    pub fn process(ctx: *Context.alice) !@This() {
+        const random: std.Random = ctx.xoshiro256.random();
+        const res: bool = random.intRangeAtMost(u32, 0, 100) < 30;
+        return .{ .resp = .{ .data = res } };
+    }
+
+    pub fn preprocess_0(ctx: *Context.coordinator, msg: @This()) !void {
+        switch (msg) {
+            .resp => |val| {
+                if (val.data) ctx += 1;
+            },
+        }
+    }
+};
+
+const BobResp = union(enum) {
+    resp: Data(bool, Check),
+
+    pub const info = two_pc(.bob, &.{.coordinator});
+
+    pub fn process(ctx: *Context.bob) !@This() {
+        const random: std.Random = ctx.xoshiro256.random();
+        const res: bool = random.intRangeAtMost(u32, 0, 100) < 30;
+        return .{ .resp = .{ .data = res } };
+    }
+
+    pub fn preprocess_0(ctx: *Context.coordinator, msg: @This()) !void {
+        switch (msg) {
+            .resp => |val| {
+                if (val.data) ctx += 1;
+            },
+        }
+    }
+};
+
+const Check = union(enum) {
+    succcessed: Data(void, ps.Exit),
+    failed_retry: Data(void, Start),
+
+    pub const info = two_pc(.coordinator, &.{ .alice, .bob });
+
+    pub fn process(ctx: *Context.coordinator) !@This() {
+        if (ctx.* == 2) {
+            return .{ .succcessed = .{ .data = {} } };
+        }
+        ctx.* = 0;
+        return .{ .failed_retry = .{ .data = {} } };
+    }
+
+    pub fn preprocess_0(ctx: *Context.alice, msg: @This()) !void {
+        _ = ctx;
+        _ = msg;
+    }
+
+    pub fn preprocess_1(ctx: *Context.bob, msg: @This()) !void {
+        _ = ctx;
+        _ = msg;
+    }
+};
